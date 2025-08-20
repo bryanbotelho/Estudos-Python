34792c671808d8c10807834d385eab599ffca2d8
Bryan <gamezer.top01@gmail.com>
2025-08-20 20:25:26 -0300

    Criado Exceção para OS

---
+++ ExtensionMove.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+def criar_diretorios(diretorios):
+    for diretorio in diretorios:
+        if not os.path.exists(diretorio):
+            try:
+                os.makedirs(diretorio)
+                print(f"Diretório {diretorio} criado.")
+            except PermissionError:
+                print(f"Sem permissão para criar o diretório {diretorio} .")
+            except Exception as e:
+                print(f"Erro inesperado ao criar {diretorio}: {e}")
+
+def mover_arquivo(diretorio_origem):
+    for arquivo in os.listdir(diretorio_origem):
+        caminho_arquivo = os.path.join(diretorio_origem, arquivo)
+        if os.path.isfile(caminho_arquivo):
+            extensao = arquivo.split('.')[-1].lower()
+            if extensao in ['pdf', 'txt', 'jpg']:
+                diretorio_destino = os.path.join(diretorio_origem, extensao)
+                try:
+                    shutil.move(caminho_arquivo, diretorio_destino)
+                    print(f"{arquivo} movido para {diretorio_destino}.")
+                except PermissionError:
+                    print(f"Sem permissão para mover {arquivo} .")
+                except Exception as e:
+                    print(f"Erro inesperado ao mover {arquivo}: {e}")
+            else:
+                print(f"Extensão {extensao} de {arquivo} não é suportada. ")
+def main():
+    diretorio_trabalho = "diretorio_trabalho"
+    diretorios = [os.path.join(diretorio_trabalho, 'pdf'),
+                  os.path.join(diretorio_trabalho, 'txt'),
+                  os.path.join(diretorio_trabalho, 'jpg')]
+    #Criar diretório
+    criar_diretorios(diretorios)
+
+    #Mover arquivos para o diretórios correspondentes
+    mover_arquivo(diretorio_trabalho)
+
+if __name__ == "__main__":
+    main()
